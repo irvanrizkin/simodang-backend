e7cee3e0ff28087eccf52ec0690a8b89f6cf7cbe
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-09-08 15:10:30 +0700

    fix: prisma alter lowercase

--- prisma/migrations/20230908062349_pond_device_id_can_empty/migration.sql
+++ prisma/migrations/20230908062349_pond_device_id_can_empty/migration.sql
@@ -1,8 +1,8 @@
 -- DropForeignKey
-ALTER TABLE `pond` DROP FOREIGN KEY `Pond_deviceId_fkey`;
+ALTER TABLE `Pond` DROP FOREIGN KEY `Pond_deviceId_fkey`;
 
 -- AlterTable
-ALTER TABLE `pond` MODIFY `deviceId` VARCHAR(191) NULL;
+ALTER TABLE `Pond` MODIFY `deviceId` VARCHAR(191) NULL;
 
 -- AddForeignKey
 ALTER TABLE `Pond` ADD CONSTRAINT `Pond_deviceId_fkey` FOREIGN KEY (`deviceId`) REFERENCES `Device`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
