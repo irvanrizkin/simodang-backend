8bb7f46ba58568b526288441327e34deb2fd5229
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-09-09 18:00:35 +0700

    feat(user): get profile from token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,7 +1,15 @@
-import { Controller, Body, Patch, UseGuards, Request } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Patch,
+  UseGuards,
+  Request,
+  Get,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { TokenGuard } from 'src/guard/token.guard';
+import { User } from '@prisma/client';
 
 @Controller('users')
 export class UsersController {
@@ -12,5 +20,11 @@ export class UsersController {
   update(@Request() req, @Body() updateUserDto: UpdateUserDto) {
     const { id } = req.user;
     return this.usersService.update(+id, updateUserDto);
+  }
+
+  @Get('/profile')
+  @UseGuards(TokenGuard)
+  GetProfile(@Request() req): Promise<User> {
+    return req.user;
   }
 }
