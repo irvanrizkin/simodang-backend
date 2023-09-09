bcf6ed37c490eb73cc56be5e2eaa013fc5a231e1
Irvan Rizki Nugraha <irvanriskinugraha90@gmail.com>
2023-09-09 22:02:13 +0700

    fix(master): only admin can add master

--- src/masters/masters.controller.ts
+++ src/masters/masters.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get, Post, Body } from '@nestjs/common';
+import { Controller, Get, Post, Body, UseGuards } from '@nestjs/common';
 import { MastersService } from './masters.service';
 import { CreateMasterDto } from './dto/create-master.dto';
+import { TokenGuard } from 'src/guard/token.guard';
+import { AdminGuard } from 'src/guard/admin.guard';
 
 @Controller('masters')
 export class MastersController {
   constructor(private readonly mastersService: MastersService) {}
 
   @Post()
+  @UseGuards(TokenGuard, AdminGuard)
   create(@Body() createMasterDto: CreateMasterDto) {
     return this.mastersService.create(createMasterDto);
   }
